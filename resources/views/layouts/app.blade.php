9a05aa7335633fb29c13db83dcd423cf559c19be
bagus564 <bagustri0823@gmail.com>
2025-06-13 20:29:50 +0700

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    {{-- BARIS INI YANG HARUS DITAMBAHKAN --}}
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
     <title>@yield('title', 'SOBAT BIMBEL')</title>
 
     <script src="https://cdn.tailwindcss.com"></script>
